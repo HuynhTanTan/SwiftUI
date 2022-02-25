947c7750bb88d84486d5a302e2d353f626724aff
Tan Huynh T <tan.huynh@monstar-lab.com>
2022-02-25 15:25:13 +0700

    Init project

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Tan Huynh T. VN.Danang on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
